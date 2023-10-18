c0a77eb125fa572aa6523a48157861191201dc84
André Luiz <andreluiz.me@gmail.com>
2023-10-18 16:05:34 -0400

    add routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
 Route::get('/', function () {
     return view('welcome');
 });
